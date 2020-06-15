f289f76a79c8269aa0b3a47fd6bc7259fc607c76
annapamma <anna.pamela@gmail.com>
2020-06-15 13:11:32 -0400

    update description

--- client/frontend/components/CptacDescription.vue
+++ client/frontend/components/CptacDescription.vue
@@ -1,17 +1,8 @@
 <template>
     <div class="cptac-description">
-    <p>NCI supported <a target="_blank"  href="https://proteomics.cancer.gov/programs/cptac">Clinical Proteomic Tumor Analysis Consortium (CPTAC)</a>,
-        is a comprehensive and coordinated effort to accelerate the understanding of the molecular
-        basis of cancer through the application of large-scale proteome (proteins) and genome (DNA and RNA)
-        analysis technologies to different cancer types. CPTAC is composed of expertise in proteomics, genomics,
-        cancer biology, oncology, and clinical chemistry, while creating open community resources that
-      are widely used by the cancer community.</p>
-      <p>Through collaboration with leaders in the field, CPTAC’s mission is to accelerate
-        precision oncology through state-of-the-art proteogenomics and create open-source community
-        resources. The <a target="_blank" href="https://cbttc.org/">Children’s Brain Tumor Tissue Consortium (CBTTC)</a> is also
-        a collaborative, multi-institutional research program dedicated to the study and
-        treatment of childhood brain tumors. The CBTTC supports the research of new prognostic
-        biomarkers and therapies for children with pediatric brain tumors.</p>
+      <p>This interactive data portal supports query, visualization, and downloads of multi-omics data from a comprehensive proteogenomic analysis of 218 tumors across seven histologic types of childhood brain cancer, a collaborative project by  <a target="_blank"  href="https://proteomics.cancer.gov/programs/cptac">Clinical Proteomic Tumor Analysis Consortium (CPTAC)</a> and <a target="_blank" href="https://cbttc.org/">Children’s Brain Tumor Tissue Consortium (CBTTC)</a>.</p>
+      <p>NCI supported CPTAC, is a comprehensive and coordinated effort to accelerate the understanding of the molecular basis of cancer through the application of large-scale proteome (proteins) and genome (DNA and RNA) analysis technologies to different cancer types. Through collaboration with leaders in the field, CPTAC’s mission is to accelerate precision oncology through state-of-the-art proteogenomics and create open-source community resources.</p>
+CBTTC is a collaborative, multi-institutional research program dedicated to the study and treatment of childhood brain tumors. The CBTTC supports the research of new prognostic biomarkers and therapies for children with pediatric brain tumors."
   </div>
 </template>
 
